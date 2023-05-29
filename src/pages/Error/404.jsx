850d8f50628debe515f36a2b89a0492851dfd697
sonuschauhan <sonusingha37fw@gmail.com>
2023-05-29 22:29:04 +0530

    fixed typo

--- src/pages/Error/404.jsx
+++ src/pages/Error/404.jsx
@@ -6,7 +6,7 @@ const Error = () => {
     <div className="error_container">
       <img className="error_img" src='./images/error.png' alt="error"></img>
       <h1 className="error_head">404</h1>
-      <p className="error_line">Oops! Page Not Found</p>
+      <p className="error_line">Oops! Page Not Found.</p>
       <Link to={"/"} className="error_btn">Go Back</Link>
     </div>
   )
