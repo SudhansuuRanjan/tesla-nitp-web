99c997e0fdcc14c3b7ce9605d385271eac197ff9
suraj_markup <sk9261712674@gmail.com>
2023-05-30 16:51:53 +0530

    about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,38 +1,13 @@
 import './About.scss'
 import Heading from '../../components/Headings/Heading'
-
-import React, { useEffect, useState } from 'react';
+import Aos from 'aos';
+import React, { useEffect, } from 'react';
 
 const About = () => {
 
-  const [visibleParagraphs, setVisibleParagraphs] = useState([]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const paragraphs = document.querySelectorAll('.scroll-down-paragraph');
-      const windowHeight = window.innerHeight;
-
-      const visible = [];
-      paragraphs.forEach((paragraph) => {
-        var paragraphOffset = 10;
-        const shouldShow = window.pageYOffset + windowHeight > paragraphOffset;
-
-        if (shouldShow) {
-          visible.push(paragraph.id);
-
-        }
-      });
-
-      setVisibleParagraphs(visible);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-
+  // useEffect(() => {
+  //       Aos.init((duration:2000));
+  // }, {});
 
 
   return (
@@ -41,31 +16,19 @@ const About = () => {
 
 
       <div className='about-container'>
-        <p>
-          Paragraph 0
+          <p className='paragraph1' data-aos="fade-up">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae officiis unde harum voluptates consequuntur. Error, non pariatur fugiat neque in asperiores! Eum in facere quos, est culpa expedita quaerat quas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos qui dolorem quos nostrum eligendi iste nulla reprehenderit consequuntur. Ex maiores quidem at facilis repudiandae rem odit aliquid obcaecati odio fugiat!
+          </p>
 
-         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, ut optio. Quas atque vero nesciunt corporis necessitatibus, voluptatibus recusandae est, laudantium, voluptates consectetur vitae quod libero illo assumenda ipsum. Aliquid?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis magni perferendis similique amet laborum illo quia provident esse quas voluptatem ullam, quis itaque. Error quis ex commodi magni minus eaque.
-        </p>
         <br />
+        <p className='paragraph2' data-aos="fade-up">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae officiis unde harum voluptates consequuntur. Error, non pariatur fugiat neque in asperiores! Eum in facere quos, est culpa expedita quaerat quas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos qui dolorem quos nostrum eligendi iste nulla reprehenderit consequuntur. Ex maiores quidem at facilis repudiandae rem odit aliquid obcaecati odio fugiat!
+          </p>
 
-        <p className={`scroll-down-paragraph ${visibleParagraphs.includes('paragraph1') ? 'visible' : ''}`} id="paragraph1">
-          Paragraph 1
-
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, ut optio. Quas atque vero nesciunt corporis necessitatibus, voluptatibus recusandae est, laudantium, voluptates consectetur vitae quod libero illo assumenda ipsum. Aliquid?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis magni perferendis similique amet laborum illo quia provident esse quas voluptatem ullam, quis itaque. Error quis ex commodi magni minus eaque.
-        </p>
-        <br />
-        <p className={`scroll-down-paragraph ${visibleParagraphs.includes('paragraph2') ? 'visible' : ''}`} id="paragraph2">
-          Paragraph 2
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, ut optio. Quas atque vero nesciunt corporis necessitatibus, voluptatibus recusandae est, laudantium, voluptates consectetur vitae quod libero illo assumenda ipsum. Aliquid?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis magni perferendis similique amet laborum illo quia provident esse quas voluptatem ullam, quis itaque. Error quis ex commodi magni minus eaque.
-        </p>
-        <br />
-
-        <p className={`scroll-down-paragraph ${visibleParagraphs.includes('paragraph3') ? 'visible' : ''}`} id="paragraph3">
-          Paragraph 3
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, ut optio. Quas atque vero nesciunt corporis necessitatibus, voluptatibus recusandae est, laudantium, voluptates consectetur vitae quod libero illo assumenda ipsum. Aliquid?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis magni perferendis similique amet laborum illo quia provident esse quas voluptatem ullam, quis itaque. Error quis ex commodi magni minus eaque.
-
-        </p>
-
+          <br />
+        <p className='paragraph3' data-aos="fade-up">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae officiis unde harum voluptates consequuntur. Error, non pariatur fugiat neque in asperiores! Eum in facere quos, est culpa expedita quaerat quas?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos qui dolorem quos nostrum eligendi iste nulla reprehenderit consequuntur. Ex maiores quidem at facilis repudiandae rem odit aliquid obcaecati odio fugiat!
+          </p>
 
       </div>
 
