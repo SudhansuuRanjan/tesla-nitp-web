58ed4ff6c04d7e2b9d8096be4c1583d125c77937
suraj_markup <sk9261712674@gmail.com>
2023-06-03 21:46:06 +0530

    Add a company partnership events page #25

---
+++ src/pages/About/About_Partners.jsx
@@ -0,0 +1,88 @@
+import "./About_Partners.scss";
+import Heading from "../../components/Headings/Heading";
+
+const AboutPartners = () => {
+  document.title = "Tesla NIT Patna | About Partners";
+
+  return (
+    <div className="pt-16 min-h-screen" >
+      <Heading heading="ABOUT OUR PARTNERS"></Heading>
+
+      <div className="paragraph-body w text-white rounded-3xl px-2 bg flex justify-items-center items-center " >
+        <div className="m-2 basis-[60%] px-2 hover:scale-95 duration-1000 ">
+          <h2 className=" text-4xl px-2 back animate-bounce  ">ALGORAND</h2>
+          <div class="max-w-[33%] mb-5 mt-1 h-1 bg-pink-500 "></div>
+          <p  >
+            Welcome to the official website of <span>T.E.S.L.A. Club</span>, the
+            premier club for Electrical Engineering enthusiasts and
+            professionals! Founded in 2021 by a group of bright-minded seniors,
+            under the guidance of our esteemed <span>PI - Dr. Amitesh Sir</span>
+            , T.E.S.L.A. Club is dedicated to fostering public speaking,
+            teamwork, technical excellence, and management skills among its
+            members. Our primary aim is to provide a platform for seniors,
+            juniors, and alumni to come together, learn, grow, and secure
+            rewarding careers in the field of Electrical Engineering.
+          </p>
+        </div>
+        <div className="m-2 basis-[40%] sm:px-5  py-2 px-2 overflow-hidden rounded-xl lg:mt-0 md:mt-0 mt-5 " d>
+          <img
+            src="https://algorand.com/static/algorand-og-image-98d634bc4a6f00c455b35830674ae96b.png"
+            className="rounded-xl w-full hover:scale-105 duration-1000 shadow-xl hue-rotate-30 "
+          ></img>
+        </div>
+      </div>
+
+      <div className="paragraph-body w text-white rounded-3xl px-2 bg flex justify-items-center items-center ">
+      <div className="m-2 basis-[40%] sm:px-5  py-2 px-2 overflow-hidden rounded-xl lg:mb-0 md:mb-0 mb-10  ">
+          <img
+            src="https://www.altcoinbuzz.io/wp-content/uploads/2022/01/what-is-near-protocol.jpg"
+            className="rounded-xl w-full hover:scale-105 duration-1000 shadow-xl hue-rotate-30 "
+          ></img>
+        </div>
+
+        <div className="m-2 basis-[60%] px-2 hover:scale-95 duration-1000 ">
+          <h2 className=" text-4xl px-2 back animate-bounce  ">NEAR</h2>
+          <div class="max-w-[17%] mb-5 mt-1 h-1 bg-pink-500 "></div>
+          <p  >
+            Welcome to the official website of <span>T.E.S.L.A. Club</span>, the
+            premier club for Electrical Engineering enthusiasts and
+            professionals! Founded in 2021 by a group of bright-minded seniors,
+            under the guidance of our esteemed <span>PI - Dr. Amitesh Sir</span>
+            , T.E.S.L.A. Club is dedicated to fostering public speaking,
+            teamwork, technical excellence, and management skills among its
+            members. Our primary aim is to provide a platform for seniors,
+            juniors, and alumni to come together, learn, grow, and secure
+            rewarding careers in the field of Electrical Engineering.
+          </p>
+        </div>
+      </div>
+
+      <div className="paragraph-body w text-white rounded-3xl px-2 bg flex justify-items-center items-center ">
+        <div className="m-2 basis-[60%] px-2 hover:scale-95 duration-1000 ">
+          <h2 className=" text-4xl px-2 back animate-bounce  ">Edu DAO</h2>
+          <div class="max-w-[25%] mb-5 mt-1 h-1 bg-pink-500 "></div>
+          <p  >
+            Welcome to the official website of <span>T.E.S.L.A. Club</span>, the
+            premier club for Electrical Engineering enthusiasts and
+            professionals! Founded in 2021 by a group of bright-minded seniors,
+            under the guidance of our esteemed <span>PI - Dr. Amitesh Sir</span>
+            , T.E.S.L.A. Club is dedicated to fostering public speaking,
+            teamwork, technical excellence, and management skills among its
+            members. Our primary aim is to provide a platform for seniors,
+            juniors, and alumni to come together, learn, grow, and secure
+            rewarding careers in the field of Electrical Engineering.
+          </p>
+        </div>
+        <div className="m-2 basis-[40%] sm:px-5  py-2 px-2 overflow-hidden rounded-xl lg:mt-0 md:mt-0 mt-5  ">
+          <img
+            src="https://pbs.twimg.com/profile_images/1470977630946181122/rXTRvqJV_400x400.jpg"
+            className="rounded-xl w-full hover:scale-105 duration-1000 shadow-xl hue-rotate-30 "
+          ></img>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default AboutPartners;
