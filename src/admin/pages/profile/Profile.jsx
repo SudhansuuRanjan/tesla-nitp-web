b57cbcb92a3d7e79c66b61f3a10e6bd6a12e13e1
sonuschauhan <sonusingha37fw@gmail.com>
2023-06-03 10:53:37 +0530

    Designed an admin profile component

--- src/admin/pages/profile/Profile.jsx
+++ src/admin/pages/profile/Profile.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import './Profile.scss'
 
 const Profile = () => {
   document.title = 'Tesla NIT Patna | Admin | Profile';
 
+
+
   return (
-    <div className='pt-16'>Profile</div>
+    <>
+      <form>
+        <div className="form">
+          <div className="details">
+            <div className='pt-16 title'>My Profile</div>
+            <label for="email" >Email:</label><br></br>
+            <input className='input' type="email" id="email" name="email" value="John"></input> <br></br>
+            <label for="name">Name:</label><br></br>
+            <input className='input' type="text" id="name" name="name" value="John"></input> <br></br>
+            </div>
+            <div className="button">
+            <button className="btn">Logout</button>
+            </div>
+        </div>
+      </form>
+    </>
+
+
+
   )
 }
 
