b5b67a7c51e5b93e35a41475d25ac9e4ed0a6c9d
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2023-10-13 10:38:27 +0530

    Update BackToTopButton.jsx

--- src/components/scroll/BackToTopButton.jsx
+++ src/components/scroll/BackToTopButton.jsx
@@ -24,14 +24,9 @@ function BackToTopButton() {
 
     return (
         <div>
-            {backToTopButton && (
-            
-                <button onClick={scrollUp} className ="fixed bottom-12 right-12 w-12 h-12 text-5xl text-white bg-sky-500  rounded-full flex items-center justify-center" > <RxPinTop size={24} /> </button>
-                
-                
-            )}
-            
-
+            <button onClick={scrollUp} className={`fixed border shadow-lg bg-opacity-70 z-50 border-gray-700 lg:bottom-12 md:bottom-12 bottom-12 lg:right-12 md:right-12 right-6 w-12 h-12 text-3xl text-gray-200 bg-gray-900 rounded-full flex items-center justify-center transition-all delay-75 ease-in-out ${!backToTopButton && "hidden"}`} > 
+                <RxPinTop size={18} /> 
+            </button>   
         </div>
     )
 }
