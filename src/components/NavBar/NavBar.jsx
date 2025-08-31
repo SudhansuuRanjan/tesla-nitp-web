f7902f1498f48f9a8bf07ff4ebf7d172fc32c589
Sudhanshu Ranjan <77230416+SudhansuuRanjan@users.noreply.github.com>
2025-08-31 15:01:08 +0530

    Change logo height in NavBar from 16 to 14
    Updated logo image height in NavBar component.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -77,10 +77,10 @@ const NavBar = () => {
     return (
         <div className="fixed top-0 z-[100] w-full">
             <div className={`w-[100%] fixed items-center justify-center ${colorChange && " bg-black transition-all delay-100  ease-in-out bg-opacity-20 backdrop-blur-md border-gray-800 shadow-lg"}`}>
-                <div className="flex items-center justify-between lg:w-[65rem] md:w-[100%] px-4 md:px-3 lg:py-5 md:py-5 py-4 m-auto text-lg">
+                <div className="flex items-center justify-between lg:w-[65rem] md:w-[100%] px-4 md:px-3 py-4 m-auto text-lg">
                     <div className="flex items-center">
                         <Link style={{ textDecoration: "none" }} to="/">
-                            <img src="/images/logo.svg" alt="logo" className="w-auto h-16" />
+                            <img src="/images/logo.svg" alt="logo" className="w-auto h-14" />
                         </Link>
                     </div>
 
